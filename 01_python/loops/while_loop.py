8a3a581c7849f7c095d385ae0a8a46a6b2f65309
MYounus-Codes <myounushere@gmail.com>
2026-07-03 03:55:39 +0500

    started while loop

---
+++ 01_python/loops/while_loop.py
@@ -0,0 +1,8 @@
+## While Loop in Python
+## A while loop is used to execute a block of code repeatedly as long as a given condition is true.
+
+i = 0
+while i < 5:
+    print(f"Current value of i: {i}")
+    i += 1  # Incrementing i to avoid infinite loop
+    
