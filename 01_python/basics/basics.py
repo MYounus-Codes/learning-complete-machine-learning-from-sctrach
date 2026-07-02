9425b67c41d8b6c468ed7787918dba5b66ea880a
MYounus-Codes <myounushere@gmail.com>
2026-07-03 03:23:26 +0500

    learning python basics

---
+++ 01_python/basics/basics.py
@@ -0,0 +1,11 @@
+## Basics :
+## Variables, Data Types, and Basic Operations and f-strings
+
+## Variables and Data Types
+# Variables are used to store data in Python. You can assign a value to a variable using the equals sign (=). Python has several built-in data types, including integers, floats, strings, and booleans.
+
+## Rules of Variable Naming:
+# 1. Variable names must start with a letter or an underscore (_).
+# 2. Variable names can contain letters, numbers, and underscores.
+# 3. Variable names are case-sensitive (e.g., myVariable and myvariable are different variables).
+
