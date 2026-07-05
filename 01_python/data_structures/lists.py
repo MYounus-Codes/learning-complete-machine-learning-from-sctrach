7ff3a2a99ba37190febf4ffc667113a34cf1ac33
MYounus-Codes <myounushere@gmail.com>
2026-07-05 13:41:06 +0500

    learning data structures

---
+++ 01_python/data_structures/lists.py
@@ -0,0 +1,10 @@
+## Lists in Python
+## Lists are one of the most versatile data structures in Python. They can hold a collection of items, which can be of different types, and they are mutable, meaning you can change their content without changing their identity.
+
+names : list = ["Muhammad", "Anees", "Younus", "Ali", "Ahmed"]
+numbers : list = [1, 2, 3, 4, 5]
+mixed : list = ["Hello", 42, 3.14, True]
+
+print("Names List:", names)
+print("Numbers List:", numbers)
+print("Mixed List:", mixed)
